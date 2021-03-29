bacdcacecaa2e1b3ffcb500b09a45d509b842139
Jake Newman <jacob.newman1@nhs.net>
2021-03-29 11:49:41 +0100

    PRMT-1664 [JN/JG] Create S3 for athena

---
+++ terraform/athena.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "athena_results" {
+  bucket = "prm-gp2gp-athena-results-${var.environment}"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "GP2GP athena results"
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+    Team        = var.team
+  }
+
+
+}
+
+resource "aws_s3_bucket_public_access_block" "athena_results_block" {
+  bucket = aws_s3_bucket.athena_results.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
